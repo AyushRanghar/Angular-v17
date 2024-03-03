f43ebfae409b5fbd6132fb58aabbb3f1175c5a4f
AYUSH RANGHAR <ayushranghar@AYUSHs-MacBook-Air.local>
2024-03-03 16:20:39 +0530

    typescript enums

--- src/app/typescript/misc.ts
+++ src/app/typescript/misc.ts
@@ -13,3 +13,36 @@ type RemoveC<TType> = TType extends 'c' ? 'd' : TType;
 // Removes the c in the uinion and replace it with D
 
 export type WowWithourC = RemoveC<Letters>;
+
+/**
+ * Nested types infer
+ */
+
+// Here you will get hint for the first and second key of the nested obj
+export const getDeepValues = <
+  Obj,
+  FirstKey extends keyof Obj,
+  SecondKey extends keyof Obj[FirstKey]
+>(
+  obj: Obj,
+  firstKey: FirstKey,
+  secondKey: SecondKey
+): Obj[FirstKey][SecondKey] => {
+  return {} as any;
+};
+
+const obj = {
+  foo: {
+    a: true,
+    b: 2,
+  },
+  bar: {
+    c: "cool",
+    d: 2,
+  }
+};
+
+const result = getDeepValues(obj, "bar", "d");
+
+console.log(result);
+
